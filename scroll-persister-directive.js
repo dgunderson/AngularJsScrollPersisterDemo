25c763e72159ed33cb91b87cc1f9c35b9d4c8356
David <gunderson.david@gmail.com>
2014-12-20 02:57:42 -0700

    removed unnecessary attributes parameter.

--- scroll-persister-directive.js
+++ scroll-persister-directive.js
@@ -6,7 +6,7 @@
 	function scrollPersister(scrollPersisterService) {
 		return {
 			restrict: "A",
-			link: function (scope, element, attributes) {
+			link: function (scope, element) {
 				element[0].scrollTop = scrollPersisterService.GetScrollTop();
 
 				scope.$on('$destroy', function () {
